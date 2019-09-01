ea2fb70c2d5ef1e9f79c85e02c0eb89426463f2b
Marlon Miles MG <magonjomarlon@gmail.com>
2019-09-01 17:28:05 +0200

    Wizard Control for Capturing User Input Data

---
+++ WebAppSharpening/Wizard Control.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppSharpening
+{
+    public partial class Wizard_Control : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        protected void Wizard1_NextButtonClick1(object sender, WizardNavigationEventArgs e)
+        {
+            if (e.NextStepIndex == 2)
+            {
+                lblFirstName.Text = txtFirstName.Text;
+                lblLastName.Text = txtLastName.Text;
+                lblGender.Text = ddlGender.SelectedValue;
+
+                lblMobile.Text = txtMobile.Text;
+                lblEmail.Text = txtEmail.Text;
+            }
+        }
+
+        protected void Wizard1_FinishButtonClick(object sender, WizardNavigationEventArgs e)
+        {
+            Response.Write("Write Code for Database Connection");
+        }
+    }
+}
